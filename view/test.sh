d9743993054e83448fb1df267664cf72b13ce5bc
yiguihai <18776473475@163.com>
2020-08-11 16:05:45 +0800

    upload

--- view/test.sh
+++
@@ -1,8 +0,0 @@
-scp /storage/emulated/0/luci-app-k3screenctrl/luasrc/controller/k3screenctrl.lua root@192.168.1.1:/usr/lib/lua/luci/controller
-scp /storage/emulated/0/luci-app-k3screenctrl/luasrc/model/cbi/k3screenctrl.lua root@192.168.1.1:/usr/lib/lua/luci/model/cbi
-scp /storage/emulated/0/luci-app-k3screenctrl/k3screenctrl/host.sh root@192.168.1.1:/root/host2.sh
-scp /storage/emulated/0/luci-app-k3screenctrl/k3screenctrl/basic.sh root@192.168.1.1:/root
-rm -rf /tmp/luci*
-a="28|nmae|ac:cd:ef"
-echo ${a##*|}
-echo ${a%%|*}
